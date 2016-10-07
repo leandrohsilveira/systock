4594a00bf133ca6ad2299726ee1fcbb4cd18be95
leandrohsilveira <leandro.hinckel@gmail.com>
2016-10-06 22:05:35 -0300

    Spring JUnit Test

--- src/main/java/senai/systock/controllers/FuncionarioController.java
+++ src/main/java/senai/systock/controllers/FuncionarioController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import senai.systock.model.Funcionario;
+
 @RestController
 public class FuncionarioController {
 	
 	@RequestMapping("/funcionario")
-	public String hello() {
-		return "Hello";
+	public Funcionario hello() {
+		return new Funcionario("Leandro", "12345678901", "Administrador");
 	}
 	
 }
