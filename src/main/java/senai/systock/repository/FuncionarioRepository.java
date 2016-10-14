19afac3bd0927f13234d760bb7eabc80ecfcd7af
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2016-10-13 22:53:50 -0300

    Ajustes no Funcionario Resource & Repository

--- src/main/java/senai/systock/repository/FuncionarioRepository.java
+++ src/main/java/senai/systock/repository/FuncionarioRepository.java
@@ -1,6 +1,10 @@
 package senai.systock.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import senai.systock.model.Funcionario;
@@ -9,4 +13,7 @@ import senai.systock.repository.projection.FuncionarioProjection;
 @RepositoryRestResource(collectionResourceRel="funcionarios" ,path="funcionarios", excerptProjection=FuncionarioProjection.class)
 public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Long> {
 
+	@Query("select f from Funcionario f where f.nome like %:nome%")
+	Page<Funcionario> findFuncionarioLike(Pageable pageable, @Param("nome") String nome);
+	
 }
--- src/main/resources/public/js/resource/funcionario.resource.js
+++ src/main/resources/public/js/resource/funcionario.resource.js
@@ -9,6 +9,20 @@
 
     /* @ngInject */
     function Funcionario($resource) {
-        return $resource('funcionarios/:id');
+		return $resource(
+			'funcionarios/:id',
+			{id: '@id'},
+			{
+				'query':  {method:'GET', isArray: false},
+				'search': {
+					url: 'funcionarios/search/:action',
+					method:'GET',
+					isArray: false,
+					params: {
+						action: '@action'
+					}
+				}
+			}
+		);
     }
 })();
