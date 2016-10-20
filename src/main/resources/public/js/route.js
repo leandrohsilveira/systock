05166ba4cec68ca4935119396d35fb04df99d376
Ivan <Ivan@192.168.0.20>
2016-10-20 10:28:54 -0200

    funcionário (view e js)

--- src/main/resources/public/js/route.js
+++ src/main/resources/public/js/route.js
@@ -41,6 +41,24 @@
 				url: '/:id',
 				controller: 'UsuarioFormController as vm',
 				templateUrl: 'paginas/usuario/usuario-form.html'
+			})
+			.state('app.funcionarios', {
+				parent: 'app',
+				url: '/funcionarios',
+				controller: 'FuncionarioListController as vm',
+				templateUrl: 'paginas/funcionario/funcionario-list.html'
+			})
+			.state('app.funcionarios.new', {
+				parent: 'app.funcionarios',
+				url: '/new',
+				controller: 'FuncionarioFormController as vm',
+				templateUrl: 'paginas/funcionario/funcionario-form.html'
+			})
+			.state('app.funcionarios.edit', {
+				parent: 'app.funcionarios',
+				url: '/:id',
+				controller: 'FuncionarioFormController as vm',
+				templateUrl: 'paginas/funcionario/funcionario-form.html'
 			});
 
 		$urlRouterProvider.otherwise("/app/index");
