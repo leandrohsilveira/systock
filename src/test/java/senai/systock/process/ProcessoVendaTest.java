b58eb5845081100368e84e4afdcc921d9662e3b3
leandrohsilveira <leandro.hinckel@gmail.com>
2016-10-20 14:32:12 -0200

    Adicionada anotação @DirtiesContext no ProcessoVendaTest - Para evitar que o teste de processo de venda suje o contexto dos demais testes, a anotação @DirtiesContext isola este teste.

--- src/test/java/senai/systock/process/ProcessoVendaTest.java
+++ src/test/java/senai/systock/process/ProcessoVendaTest.java
@@ -1,14 +1,15 @@
 package senai.systock.process;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import senai.systock.TestApplication;
 import senai.systock.model.Cargo;
@@ -25,61 +26,62 @@ import senai.systock.repository.VendaRepository;
 import senai.systock.utils.CPFUtils;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes=TestApplication.class)
+@SpringBootTest(classes = TestApplication.class)
 @AutoConfigureMockMvc
+@DirtiesContext
 public class ProcessoVendaTest {
-	
+
 	private static final int QUANTIDADE_ITEM = 2;
 	private static final Float VALOR_DESCONTO = 3.5f;
 	private static final Float VALOR_PRODUTO = 325.33f;
 	private static final Float VALOR_SUBTOTAL = VALOR_PRODUTO * QUANTIDADE_ITEM;
 	private static final Float VALOR_TOTAL = VALOR_SUBTOTAL - (VALOR_SUBTOTAL * (VALOR_DESCONTO / 100));
-	
+
 	@Autowired
 	private VendaRepository vendaRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private FuncionarioRepository funcionarioRepository;
-	
+
 	@Test
 	public void sucessoTest() {
 		Funcionario funcionario = funcionarioRepository.save(new Funcionario("Vendedor 1", CPFUtils.gerarCPF(), Cargo.VENDEDOR));
 		assertNotNull(funcionario);
 		assertNotNull(funcionario.getId());
-		
+
 		Venda venda = vendaRepository.save(new Venda(funcionario, SituacaoVenda.ABERTA));
 		assertNotNull(venda);
 		assertNotNull(venda.getId());
 		assertEquals(SituacaoVenda.ABERTA, venda.getSituacao());
-		
+
 		Cliente cliente = clienteRepository.save(new Cliente("Cliente 1", CPFUtils.gerarCPF(), "cliente1@gmail.com"));
 		assertNotNull(cliente);
 		assertNotNull(cliente.getId());
-		
+
 		Produto produto = produtoRepository.save(new Produto("Produto 1", VALOR_PRODUTO, 5));
 		assertNotNull(produto);
 		assertNotNull(produto.getId());
-		
+
 		venda.setCliente(cliente);
 		venda.setDesconto(VALOR_DESCONTO);
 		venda.addItemVenda(new ItemVenda(venda, produto, VALOR_PRODUTO, QUANTIDADE_ITEM));
-		
+
 		venda = vendaRepository.save(venda);
 		assertNotNull(venda);
 		assertEquals(VALOR_SUBTOTAL, venda.getSubtotal());
 		assertEquals(VALOR_TOTAL, venda.getTotal());
 		assertEquals(SituacaoVenda.ABERTA, venda.getSituacao());
-		
+
 		venda.setSituacao(SituacaoVenda.CONCLUIDA);
 		venda = vendaRepository.save(venda);
 		assertEquals(SituacaoVenda.CONCLUIDA, venda.getSituacao());
-		
+
 	}
-	
+
 }
