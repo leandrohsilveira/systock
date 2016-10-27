f73a7dcee6e40d4ecd09c5ac860a1d8498f42226
leandrohsilveira <leandro.hinckel@gmail.com>
2016-10-27 17:21:52 -0200

    #5 Método "ValidacaoClienteTest.emailDuplicadoTest() removido

--- src/test/java/senai/systock/model/ValidacaoClienteTest.java
+++ src/test/java/senai/systock/model/ValidacaoClienteTest.java
@@ -9,71 +9,58 @@ import java.util.Set;
 import javax.validation.ConstraintViolation;
 import javax.validation.ValidationException;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-
 public class ValidacaoClienteTest {
-	
+
 	@Test
 	public void sucessoTest() throws ValidationException {
-		Cliente cliente = new Cliente("Leandro","76042714210","leandro@gmail.com");
+		Cliente cliente = new Cliente("Leandro", "76042714210", "leandro@gmail.com");
 		Set<ConstraintViolation<Object>> validacoes = cliente.validar();
 		assertNotNull(validacoes);
 		assertTrue(validacoes.isEmpty());
 	}
-	
+
 	@Test
 	public void cpfInvalidoTest() {
-		Cliente cliente = new Cliente("Leandro 1","29697822941","leandro1@gmail.com");
+		Cliente cliente = new Cliente("Leandro 1", "29697822941", "leandro1@gmail.com");
 		Set<ConstraintViolation<Object>> validar = cliente.validar();
 		assertNotNull(validar);
 		assertTrue(validar.size() == 1);
 		String message = validar.iterator().next().getMessage();
 		assertEquals("O CPF é inválido", message);
 	}
-	
-	@Test
-	@Ignore
-	public void emailDuplicadoTest() {
-		Cliente cliente = new Cliente("Leandro 1","55587135853","cliente.teste@gmail.com");	
-		Set<ConstraintViolation<Object>> validar = cliente.validar();
-		assertNotNull(validar);
-		assertTrue(validar.size() == 1);
-		String message = validar.iterator().next().getMessage();
-		assertEquals("O E-mail já está cadastrado para um cliente", message);
-	}
-	
+
 	@Test
 	public void emailInvalidoTest1() {
-		Cliente cliente = new Cliente("Leandro 1","55587135853","leandro@");
+		Cliente cliente = new Cliente("Leandro 1", "55587135853", "leandro@");
 		Set<ConstraintViolation<Object>> validar = cliente.validar();
 		assertNotNull(validar);
 		assertTrue(validar.size() == 1);
 		String message = validar.iterator().next().getMessage();
 		assertEquals("O E-mail é inválido", message);
 	}
-	
+
 	@Test
 	public void emailInvalidoTest2() {
-		Cliente cliente = new Cliente("Leandro 1","55587135853","@gmail.com");	
+		Cliente cliente = new Cliente("Leandro 1", "55587135853", "@gmail.com");
 		Set<ConstraintViolation<Object>> validar = cliente.validar();
 		assertNotNull(validar);
 		assertTrue(validar.size() == 1);
 		String message = validar.iterator().next().getMessage();
 		assertEquals("O E-mail é inválido", message);
 	}
-	
+
 	@Test
 	public void nomeVazioTest() {
-		Cliente cliente = new Cliente(null, "55587135853","leandro1@gmail.com");
+		Cliente cliente = new Cliente(null, "55587135853", "leandro1@gmail.com");
 		Set<ConstraintViolation<Object>> validar = cliente.validar();
 		assertNotNull(validar);
 		assertTrue(validar.size() == 1);
 		String message = validar.iterator().next().getMessage();
 		assertEquals("O Nome é obrigatório", message);
 	}
-	
+
 	@Test
 	public void cpfVazioTest() {
 		Cliente cliente = new Cliente("Leandro 1", null, "leandro1@gmail.com");
@@ -83,10 +70,10 @@ public class ValidacaoClienteTest {
 		String message = validar.iterator().next().getMessage();
 		assertEquals("O CPF é obrigatório", message);
 	}
-	
+
 	@Test
 	public void emailVazioTest() {
-		Cliente cliente = new Cliente("Leandro 1", "55587135853", null);	
+		Cliente cliente = new Cliente("Leandro 1", "55587135853", null);
 		Set<ConstraintViolation<Object>> validar = cliente.validar();
 		assertNotNull(validar);
 		assertTrue(validar.size() == 1);
